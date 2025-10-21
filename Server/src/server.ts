6ea03742474f2644520713b0ebab26140c348427
vietduc-dev <levietduc.dev@gmail.com>
2025-10-21 18:46:34 +0700

    server-chore: config cloudinary

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,5 +1,12 @@
 import app from "./app";
 import { config } from "./config/app.config";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: config.CLOUDINARY_CLIENT_NAME,
+  api_key: config.CLOUDINARY_CLIENT_API,
+  api_secret: config.CLOUDINARY_CLIENT_SECRET,
+});
 
 app.listen(config.PORT, () => {
   console.log(`Server is running on port ${config.PORT}`);
